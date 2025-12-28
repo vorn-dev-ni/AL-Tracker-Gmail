0cf6ccce1d9f9ec0116384b0af47e88c227182f8
vorn-tovtrip <sovanpanhavorn.siv@tovtrip.com>
2025-12-28 13:49:06 +0700

    fix: access token expired

--- src/auth.ts
+++ src/auth.ts
@@ -11,18 +11,33 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
           prompt: "consent",
         },
       },
-    }), 
+    }),
   ],
   callbacks: {
     async jwt({ token, account }) {
+      // Initial sign in
       if (account) {
-        token.accessToken = account.access_token
+        return {
+          ...token,
+          accessToken: account.access_token,
+          expiresAt: Date.now() + (account.expires_in as number) * 1000,
+          refreshToken: account.refresh_token,
+        }
       }
-      return token
+
+      // Return previous token if the access token has not expired yet
+      if (Date.now() < (token.expiresAt as number)) {
+        return token
+      }
+
+      // Access token has expired, try to update it
+      return refreshAccessToken(token)
     },
     async session({ session, token }) {
       // @ts-ignore
       session.accessToken = token.accessToken
+      // @ts-ignore
+      session.error = token.error
       return session
     },
   },
@@ -35,3 +50,44 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   },
   trustHost: true,
 })
+
+async function refreshAccessToken(token: any) {
+  try {
+    const url =
+      "https://oauth2.googleapis.com/token?" +
+      new URLSearchParams({
+        client_id: process.env.AUTH_GOOGLE_ID!,
+        client_secret: process.env.AUTH_GOOGLE_SECRET!,
+        grant_type: "refresh_token",
+        refresh_token: token.refreshToken,
+      })
+
+    const response = await fetch(url, {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      method: "POST",
+    })
+
+    const refreshedTokens = await response.json()
+
+    if (!response.ok) {
+      throw refreshedTokens
+    }
+
+    return {
+      ...token,
+      accessToken: refreshedTokens.access_token,
+      expiresAt: Date.now() + refreshedTokens.expires_in * 1000,
+      refreshToken: refreshedTokens.refresh_token ?? token.refreshToken, // Fall back to old refresh token
+    }
+  } catch (error) {
+    console.error("Error refreshing access token", error)
+
+    return {
+      ...token,
+      error: "RefreshAccessTokenError",
+    }
+  }
+}
+
